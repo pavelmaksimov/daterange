b2acc6d8a67949f0e0da3e1102d9579f4b7d27f2
pmaksimov <vur21@ya.ru>
2018-11-29 17:41:51 +0300

    change setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     name='daterange',
     version=VERSION,
     packages=['daterange'],
-    install_requires=['pandas'],
+    install_requires=['pandas', 'dateutil'],
     license='MIT',
     url='https://github.com/pavelmaksimov/daterange',
     author='Pavel Maksimov',
