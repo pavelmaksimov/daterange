1bbb7cee71091a6cb83b32428bfd677700ae4b23
pmaksimov <vur21@ya.ru>
2018-11-30 10:56:09 +0300

    change setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = '29.11.18'
+VERSION = '30.11.18'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Генерация периодов",
     long_description=long_description,
     packages=['daterange'],
-    install_requires=['pandas', 'dateutil'],
+    install_requires=['pandas', 'python-dateutil'],
     license='MIT',
     url='https://github.com/pavelmaksimov/daterange',
     author='Pavel Maksimov',
